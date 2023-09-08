17c2c6f8b90d927578d5e680d4e64ec2f742ddce
codedmachine111 <charlesbabbage1709@gmail.com>
2023-09-09 01:26:13 +0530

    feat: uuid integration

--- routes/Room.js
+++ routes/Room.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const router = express.Router();
 require('dotenv').config();
-const livekitApi = require("livekit-server-sdk");
+
 const { validateToken } = require("../middlewares/AuthMiddleware");
+
+const livekitApi = require("livekit-server-sdk");
 const AccessToken = livekitApi.AccessToken;
 
+const  { v4: uuidv4, v4} = require('uuid');
+
 router.get("/get-token", validateToken, async (req, res) => {
-  const { roomId, name } = req.query;
+  const { roomName, username } = req.query;
 
   const at = new AccessToken(
     process.env.LIVEKIT_API_KEY,      
     process.env.LIVEKIT_SECRET_KEY,   
     {
-      identity: name,
+      identity: username,
     }
   );
+
+  const roomId = roomName + '-' + v4();
+
   at.addGrant({ roomJoin: true, room: roomId });
 
   const token = at.toJwt();
-  res.json({ token: token });
+  res.json({ token: token, roomId: roomId });
 });
 
 module.exports = router;
